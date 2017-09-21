67499aa2c631bddd0281b7629b39411bd052668e
AnnatarHe <iamhele1994@gmail.com>
2017-09-22 00:09:52 +0800

    add apollo fail

---
+++ app/src/main/java/com/annatarhe/athena/Model/Config.java
@@ -0,0 +1,14 @@
+package com.annatarhe.athena.Model;
+
+/**
+ * Created by Annatarhe on 9/22/2017.
+ *
+ * @author AnnatarHe
+ * @email iamhele1994@gmail.com
+ */
+
+
+
+public class Config {
+    public static ApolloClient apolloClient = ApolloClient.builder();
+}
